473a36750edb9c99607e257843029afcdb6a0211
João Fellipefelps <joao_jfv@hotmail.com>
2018-12-04 21:07:48 -0200

    fjdjf

---
+++ infonote_07/src/model/Usuario.java
@@ -0,0 +1,52 @@
+package model;
+
+public abstract class Usuario {
+	private String login;
+
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Usuario(String login, String senha, int tipo) {
+		super();
+		this.login = login;
+		this.senha = senha;
+		this.tipo = tipo;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	private String senha;
+	private int tipo;
+
+	@Override
+	public String toString() {
+		return "Usuario [login=" + login + ", senha=" + senha + ", tipo=" + tipo + ", getLogin()=" + getLogin()
+				+ ", getSenha()=" + getSenha() + ", getTipo()=" + getTipo() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+
+}
